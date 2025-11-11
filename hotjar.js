e714d8981e1e430deb42dc9c76675029b0b9d3ad
Sebastian <sebbs21@gmail.com>
2025-11-11 14:03:38 -0600

    Update Hotjar to use @hotjar/browser module in all HTML files

---
+++ hotjar.js
@@ -0,0 +1,8 @@
+// Hotjar initialization using @hotjar/browser
+import Hotjar from 'https://cdn.jsdelivr.net/npm/@hotjar/browser@1.0.0/dist/index.js';
+
+const siteId = 6573027;
+const hotjarVersion = 6;
+
+Hotjar.init(siteId, hotjarVersion);
+
